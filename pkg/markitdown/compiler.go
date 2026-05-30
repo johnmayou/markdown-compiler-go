5f6fbb03f0e2c532a1e4e6c697b9504340c12a2f
John Mayou <john@johnmayou.com>
2026-05-29 23:09:28 -0500

    feat: initial pkg

---
+++ pkg/markitdown/compiler.go
@@ -0,0 +1,5 @@
+package markitdown
+
+func Add(x int, y int) int {
+	return x + y
+}
---
+++ pkg/markitdown/compiler_test.go
@@ -0,0 +1,11 @@
+package markitdown
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAdd(t *testing.T) {
+	require.Equal(t, 2, Add(1, 1))
+}
